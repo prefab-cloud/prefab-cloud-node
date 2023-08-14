9bd17c64b64050fdfe97ba643a24b2060deed07c
Jeffrey Chupp <jeff@semanticart.com>
2023-08-14 12:46:47 -0400

    Make fetch implementation more flexible...
    This allows using node-fetch, etc. properly in Nodes that don't provide
    their own fetch.

--- src/makeHeaders.ts
+++ src/makeHeaders.ts
@@ -1,9 +1,12 @@
 import { makeToken } from "./makeToken";
 
+const version: string = require("../package.json").version;
+
 export const makeHeaders = (apiKey: string): Record<string, string> => {
   const token = makeToken(apiKey);
 
   return {
     Authorization: `Basic ${token}`,
+    "X-PrefabCloud-Client-Version": `prefab-cloud-node-${version}`,
   };
 };
