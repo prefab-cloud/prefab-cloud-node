be4ba8f86ed5d833b2f56bd2025cdb2647d3dcb8
Jeffrey Chupp <jeff@semanticart.com>
2024-01-04 13:42:26 -0500

    feat: .logger
    `.logger` takes a logger name and desiredLevel (optional if set at the
    Prefab instance-level) and returns an object with the following methods:
    
    - trace
    - debug
    - info
    - warn
    - error
    - fatal
    
    Example usage:
    
    ```typescript
    const logger = prefab.logger("netlify.functions.example", "info");
    
    logger.debug("This will not up") // does not log
    logger.info("This will show up") // logs INFO  netlify.functions.example: This will show up
    logger.error("This will show up") // logs ERROR netlify.functions.example: This will show up
    ```

--- src/logger.ts
+++ src/logger.ts
@@ -3,13 +3,16 @@ import { type Resolver } from "./resolver";
 
 export const PREFIX = "log-level.";
 
-export type ValidLogLevelName =
-  | "trace"
-  | "debug"
-  | "info"
-  | "warn"
-  | "error"
-  | "fatal";
+const validLogLevelNames = [
+  "trace",
+  "debug",
+  "info",
+  "warn",
+  "error",
+  "fatal",
+] as const;
+
+export type ValidLogLevelName = (typeof validLogLevelNames)[number];
 
 export type ValidLogLevel = 1 | 2 | 3 | 5 | 6 | 9;
 
@@ -76,4 +79,64 @@ export const shouldLog = ({
   }
 
   return defaultLevel <= desiredLevel;
+};
+
+type MadeLogger = Record<
+  ValidLogLevelName,
+  (
+    message: unknown,
+    contexts?: Contexts | ContextObj | undefined
+  ) => string | undefined
+>;
+
+export const makeLogger = ({
+  loggerName,
+  defaultLevel,
+  resolver,
+}: {
+  loggerName: string;
+  defaultLevel: ValidLogLevel;
+  resolver: Resolver;
+}): MadeLogger => {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  const logger = {} as MadeLogger;
+
+  validLogLevelNames.forEach((levelName) => {
+    const desiredLevel = parseLevel(levelName);
+
+    if (desiredLevel === undefined) {
+      throw new Error(`Invalid level: ${levelName}`);
+    }
+
+    const printableName = (
+      levelName + " ".repeat(5 - levelName.length)
+    ).toUpperCase();
+
+    logger[levelName] = (
+      message: unknown,
+      contexts?: Contexts | ContextObj | undefined
+    ) => {
+      if (
+        shouldLog({
+          loggerName,
+          desiredLevel,
+          defaultLevel,
+          resolver,
+          contexts,
+        })
+      ) {
+        const printableMessage =
+          typeof message === "string" ? message : JSON.stringify(message);
+
+        const output = `${printableName} ${loggerName}: ${printableMessage}`;
+        console.log(output);
+
+        return output;
+      } else {
+        return undefined;
+      }
+    };
+  });
+
+  return logger;
 };
